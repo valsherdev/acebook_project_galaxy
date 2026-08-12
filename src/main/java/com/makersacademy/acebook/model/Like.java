39d11605a3da521350ec69a0502e3868e92ba739
LeraSher <LeraSher>
2026-08-12 10:56:08 +0100

    Likes feature initial commit

---
+++ src/main/java/com/makersacademy/acebook/model/Like.java
@@ -0,0 +1,13 @@
+package com.makersacademy.acebook.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "LIKES")
+public class Like {
+    
+
+}
