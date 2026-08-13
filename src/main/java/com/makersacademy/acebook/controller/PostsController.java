79ea882cdb315cc76e57fa7e8c52f98ddbe8fbe5
Trouli <99565045+petroulakambouri@users.noreply.github.com>
2026-08-13 10:38:36 +0100

    set up delete method in controller

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -68,6 +68,16 @@ public class PostsController {
         return "posts/show";
     }
 
+    @PostMapping("/posts/{postId}/delete")
+    public RedirectView delete(@PathVariable Long postId) {
+        Post post = repository.findById(postId).orElseThrow();
+        User currentUser = getCurrentUser();
+        if (post.getUser().getId().equals(currentUser.getId())) {
+            repository.delete(post);
+        }
+        return new RedirectView("/posts");
+    }
+
     @PostMapping("/posts/{postId}/likes")
     public RedirectView toggleLike(@PathVariable Long postId) {
         Post post = repository.findById(postId).orElseThrow();
