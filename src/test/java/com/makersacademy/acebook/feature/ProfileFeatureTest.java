22938fe821c94d1f6964d2c448c081ac8e77317a
naimaahsan <naimauk@yahoo.co.uk>
2026-08-14 18:33:08 +0100

    added 4 extra profile feature tests, updated all the tests as some were failing due to selenuim not logging in after registering so created helper method and updated tests

--- src/test/java/com/makersacademy/acebook/feature/ProfileFeatureTest.java
+++ src/test/java/com/makersacademy/acebook/feature/ProfileFeatureTest.java
@@ -21,12 +21,14 @@ import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 public class ProfileFeatureTest {
     WebDriver driver;
     Faker faker;
+    WebDriverWait wait;
 
     @BeforeEach
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
         driver = new ChromeDriver();
         faker = new Faker();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterEach
@@ -36,131 +38,248 @@ public class ProfileFeatureTest {
         }
     }
 
-    @Test
-    public void testGoingToProfilePageRendersEmptyProfile() {
-        String email = faker.name().username() + "@email.com";
-
+    // creates an account, and if it doesn't log in automatically, it will log in manually right after
+    // - this was causing some of the tests to fail because it wasn't logging the test in after creating the account so
+    // it couldn't find the next steps specified on the tests
+    private void signUpAndLogInUser(String email, String password) {
         driver.get("http://localhost:8081/");
         driver.findElement(By.linkText("Sign up")).click();
         driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("password")).sendKeys("P@55qw0rd");
+        driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.name("action")).click();
 
-        driver.findElement(By.linkText("Profile")).click();
-        driver.get("http://localhost:8081/profile");
+        // manual log in, if not done automatically
+        if (driver.getCurrentUrl().contains("/login")) {
+            driver.findElement(By.name("email")).sendKeys(email);
+            driver.findElement(By.name("password")).sendKeys(password);
+            driver.findElement(By.tagName("button")).click();
+        }
+    }
 
-        driver.findElement(By.xpath("/html/body/div[2]/div/p[2]/strong"));
+    // User can see an empty profile page when they go onto their profile before filling anything out
+    @Test
+    public void testGoingToProfilePageRendersEmptyProfile() {
+        String email = faker.name().username() + "@email.com";
+        signUpAndLogInUser(email, "P@55qw0rd");
+
+        driver.get("http://localhost:8081/profile");
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), "First name:"));
 
         String pageText = driver.findElement(By.tagName("body")).getText();
         assertTrue(pageText.contains("First name:"));
-
-
     }
 
+    // First name saved successfully to profile
     @Test
     public void testUpdatingProfileSavesFirstNameAndRendersOnProfilePage() {
         String email = faker.name().username() + "@email.com";
         String firstName = faker.name().firstName();
+        signUpAndLogInUser(email, "P@55qw0rd");
 
-        driver.get("http://localhost:8081/");
-        driver.findElement(By.linkText("Sign up")).click();
-        driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("password")).sendKeys("P@55qw0rd");
-        driver.findElement(By.name("action")).click();
-
-        driver.findElement(By.linkText("Profile")).click();
         driver.get("http://localhost:8081/profile");
-
-        driver.findElement(By.linkText("Edit Profile")).click();
         driver.get("http://localhost:8081/profile/edit");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), "First name:"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("firstName")));
 
-        driver.findElement(By.xpath("//*[@id=\"firstName\"]")).click();
         driver.findElement(By.name("firstName")).sendKeys(firstName);
         driver.findElement(By.tagName("button")).click();
+
         driver.get("http://localhost:8081/profile");
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), firstName));
 
         String pageText = driver.findElement(By.tagName("body")).getText();
         assertTrue(pageText.contains("First name: " + firstName));
     }
 
+    // First and last name saved successfully to profile
     @Test
     public void testUpdatingProfileSavesFirstAndLastNameAndRendersOnProfilePage() {
         String email = faker.name().username() + "@email.com";
         String firstName = faker.name().firstName();
         String lastName = faker.name().lastName();
+        signUpAndLogInUser(email, "P@55qw0rd");
 
-        driver.get("http://localhost:8081/");
-        driver.findElement(By.linkText("Sign up")).click();
-        driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("password")).sendKeys("P@55qw0rd");
-        driver.findElement(By.name("action")).click();
-
-        driver.findElement(By.linkText("Profile")).click();
         driver.get("http://localhost:8081/profile");
-
-        driver.findElement(By.linkText("Edit Profile")).click();
         driver.get("http://localhost:8081/profile/edit");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), "First name:"));
 
-        driver.findElement(By.xpath("//*[@id=\"firstName\"]")).click();
         driver.findElement(By.name("firstName")).sendKeys(firstName);
-        driver.findElement(By.xpath("//*[@id=\"lastName\"]")).click();
         driver.findElement(By.name("lastName")).sendKeys(lastName);
         driver.findElement(By.tagName("button")).click();
+
         driver.get("http://localhost:8081/profile");
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), firstName));
 
         String pageText = driver.findElement(By.tagName("body")).getText();
         assertTrue(pageText.contains("First name: " + firstName));
         assertTrue(pageText.contains("Last name: " + lastName));
     }
 
+    //Both first and last name filled out on profile, full name shows on users post
     @Test
     public void testUpdatingProfileSavesFirstAndLastNameAndRendersOnPostPage() {
         String email = faker.name().username() + "@email.com";
         String newPost = faker.lorem().sentence();
         String firstName = faker.name().firstName();
         String lastName = faker.name().lastName();
+        signUpAndLogInUser(email, "P@55qw0rd");
 
-
-        driver.get("http://localhost:8081/");
-        driver.findElement(By.linkText("Sign up")).click();
-        driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("password")).sendKeys("P@55qw0rd");
-        driver.findElement(By.name("action")).click();
+        driver.get("http://localhost:8081/posts");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("content")));
 
         driver.findElement(By.name("content")).sendKeys(newPost);
         driver.findElement(By.cssSelector("input[type='submit']")).click();
 
-        driver.findElement(By.linkText("Profile")).click();
         driver.get("http://localhost:8081/profile");
-
-        driver.findElement(By.linkText("Edit Profile")).click();
         driver.get("http://localhost:8081/profile/edit");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), "First name:"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("firstName")));
 
-        driver.findElement(By.xpath("//*[@id=\"firstName\"]")).click();
         driver.findElement(By.name("firstName")).sendKeys(firstName);
-        driver.findElement(By.xpath("//*[@id=\"lastName\"]")).click();
         driver.findElement(By.name("lastName")).sendKeys(lastName);
         driver.findElement(By.tagName("button")).click();
+
         driver.get("http://localhost:8081/profile");
 
         String pageText = driver.findElement(By.tagName("body")).getText();
         assertTrue(pageText.contains("First name: " + firstName));
         assertTrue(pageText.contains("Last name: " + lastName));
 
-        driver.findElement(By.linkText("Home")).click();
         driver.get("http://localhost:8081/posts");
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), firstName));
 
         String pageBodyText = driver.findElement(By.tagName("body")).getText();
         assertThat(pageBodyText, containsString(firstName + ' ' + lastName));
 
     }
+
+    // Only last name filled on profile, posts show users email on post (as full name is incomplete)
+    @Test
+    public void testUpdatingOnlyLastNameRendersOnPostPageAsEmailAndNotIncompleteName() {
+        String email = faker.name().username() + "@email.com";
+        String newPost = faker.lorem().sentence();
+        String lastName = faker.name().lastName();
+        signUpAndLogInUser(email, "P@55qw0rd");
+
+        driver.get("http://localhost:8081/posts");
+        driver.findElement(By.name("content")).sendKeys(newPost);
+        driver.findElement(By.cssSelector("input[type='submit']")).click();
+
+        driver.get("http://localhost:8081/profile");
+        driver.get("http://localhost:8081/profile/edit");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("lastName")));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), "First name:"));
+
+        driver.findElement(By.name("lastName")).sendKeys(lastName);
+        driver.findElement(By.tagName("button")).click();
+
+        driver.get("http://localhost:8081/profile");
+
+        String pageText = driver.findElement(By.tagName("body")).getText();
+        assertTrue(pageText.contains("Last name: " + lastName));
+
+        driver.get("http://localhost:8081/posts");
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), email));
+
+        String pageBodyText = driver.findElement(By.tagName("body")).getText();
+        assertThat(pageBodyText, containsString(email));
+
+    }
+
+    // Only first name filled on profile, posts show users email on post (as full name is incomplete)
+    @Test
+    public void testUpdatingOnlyFirstNameRendersOnPostPageAsEmailAndNotIncompleteName() {
+        String email = faker.name().username() + "@email.com";
+        String newPost = faker.lorem().sentence();
+        String firstName = faker.name().firstName();
+        signUpAndLogInUser(email, "P@55qw0rd");
+
+        driver.get("http://localhost:8081/posts");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("content")));
+
+        driver.findElement(By.name("content")).sendKeys(newPost);
+        driver.findElement(By.cssSelector("input[type='submit']")).click();
+
+        driver.get("http://localhost:8081/profile");
+        driver.get("http://localhost:8081/profile/edit");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("firstName")));
+
+        driver.findElement(By.name("firstName")).sendKeys(firstName);
+        driver.findElement(By.tagName("button")).click();
+
+        driver.get("http://localhost:8081/profile");
+
+        String pageText = driver.findElement(By.tagName("body")).getText();
+        assertTrue(pageText.contains("First name: " + firstName));
+
+        driver.get("http://localhost:8081/posts");
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), email));
+
+        String pageBodyText = driver.findElement(By.tagName("body")).getText();
+        assertThat(pageBodyText, containsString(email));
+
+    }
+
+    // When a user uploads a profile picture, it saves and shows on their profile page
+    @Test
+    public void testUploadingAnImageRendersCorrectlyOnProfilePage() {
+        String email = faker.name().username() + "@email.com";
+        signUpAndLogInUser(email, "P@55qw0rd");
+
+        driver.get("http://localhost:8081/profile");
+        driver.get("http://localhost:8081/profile/edit");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("imageFile")));
+
+        String imagePath = java.nio.file.Paths.get("src/test/java/com/makersacademy/acebook/resources/test-image.jpeg").toAbsolutePath().toString();
+        driver.findElement(By.name("imageFile")).sendKeys(imagePath);
+
+        driver.findElement(By.tagName("button")).click();
+        driver.get("http://localhost:8081/profile");
+
+        WebElement profileImage = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("img.profile-pic-placeholder")));
+        assertThat(profileImage.getAttribute("src"), containsString("data:image/"));
+        }
+
+    // When a user makes a post and also uploads a profile picture, their profile picture will show on their
+    // individual posts
+    @Test
+    public void testAddingAPostAndUploadingAProfilePictureRendersCorrectlyOnPostPage() {
+        String email = faker.name().username() + "@email.com";
+        String newPost = faker.lorem().sentence();
+        signUpAndLogInUser(email, "P@55qw0rd");
+
+        driver.get("http://localhost:8081/posts");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("content")));
+
+        driver.findElement(By.name("content")).sendKeys(newPost);
+        driver.findElement(By.cssSelector("input[type='submit']")).click();
+
+        driver.get("http://localhost:8081/profile");
+        driver.get("http://localhost:8081/profile/edit");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("imageFile")));
+
+        String imagePath = java.nio.file.Paths.get("src/test/java/com/makersacademy/acebook/resources/test-image.jpeg").toAbsolutePath().toString();
+        driver.findElement(By.name("imageFile")).sendKeys(imagePath);
+        driver.findElement(By.tagName("button")).click();
+        driver.get("http://localhost:8081/profile");
+
+        WebElement profileImage = driver.findElement(By.cssSelector("img.profile-pic-placeholder"));
+        assertThat(profileImage.getAttribute("src"), containsString("data:image/"));
+
+        driver.get("http://localhost:8081/posts");
+
+        String pageBodyText = driver.findElement(By.tagName("body")).getText();
+        assertThat(pageBodyText, containsString(email));
+
+        WebElement postProfileImage = driver.findElement(By.cssSelector("li.post-box img.profile-pic-placeholder"));
+        assertThat(postProfileImage.getAttribute("src"), containsString("data:image/"));
+    }
+
 }
