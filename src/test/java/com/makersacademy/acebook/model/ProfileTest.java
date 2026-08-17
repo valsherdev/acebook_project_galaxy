9fa77d3b90913459b1469a7f3a303c035b613072
naimaahsan <naimauk@yahoo.co.uk>
2026-08-17 12:21:54 +0100

    added model tests to improve coveage by covering lombok methods

--- src/test/java/com/makersacademy/acebook/model/ProfileTest.java
+++ src/test/java/com/makersacademy/acebook/model/ProfileTest.java
@@ -20,6 +20,11 @@ public class ProfileTest {
     }
 
     @Test
+    public void testCanGetId() {
+        assertThat(profile.getId(), is(1L));
+    }
+
+    @Test
     public void testCanGetFirstName() {
         assertThat(profile.getFirstName(), containsString("Test"));
     }
@@ -50,6 +55,67 @@ public class ProfileTest {
         Profile emptyProfile = new Profile();
         assertThat(emptyProfile.convertImageByteToString(), nullValue());
     }
+
+    @Test
+    public void testCanGetAboutMe() {
+        assertThat(profile.getAboutMe(), containsString("I am a test"));
+    }
+
+    @Test
+    public void testAllArgsConstructorAndGetters() {
+        assertThat(profile.getId(), is(1L));
+        assertThat(profile.getFirstName(), is("Test"));
+        assertThat(profile.getLastName(), is("User"));
+        assertThat(profile.getCurrentLocation(), is("Southampton"));
+        assertThat(profile.getHometown(), is("Newcastle"));
+        assertThat(profile.getProfilePicture(), is(dummyBytes));
+        assertThat(profile.getAboutMe(), is("I am a test"));
+        assertThat(profile.getUser().getUsername(), is("testuser@test.com"));
+
+    }
+
+    // tests lombok getter and setter methods
+    @Test
+    public void testNoArgsConstructorAndSetters() {
+        Profile emptyProfile = new Profile();
+        emptyProfile.setId(2L);
+        emptyProfile.setFirstName("Jane");
+        emptyProfile.setLastName("Doe");
+        emptyProfile.setCurrentLocation("London");
+        emptyProfile.setHometown("Manchester");
+        emptyProfile.setProfilePicture(dummyBytes);
+        emptyProfile.setAboutMe("Hello world");
+        emptyProfile.setUser(user);
+
+        assertThat(emptyProfile.getId(), is(2L));
+        assertThat(emptyProfile.getFirstName(), is("Jane"));
+        assertThat(emptyProfile.getLastName(), is("Doe"));
+        assertThat(emptyProfile.getCurrentLocation(), is("London"));
+        assertThat(emptyProfile.getHometown(), is("Manchester"));
+        assertThat(emptyProfile.getProfilePicture(), is(dummyBytes));
+        assertThat(emptyProfile.getAboutMe(), is("Hello world"));
+        assertThat(emptyProfile.getUser(), is(user));
+    }
+
+    // tests lomboks three special helper methods that Lombok automatically creates for the Profile
+    // class behind the scenes: toString(), equals(), and hashCode()
+    @Test
+    public void testLombokToStringEqualsAndHashCode() {
+        Profile p1 = new Profile(1L, "Test", "User", "Southampton", "Newcastle", dummyBytes, "I am a test", user);
+        Profile p2 = new Profile(1L, "Test", "User", "Southampton", "Newcastle", dummyBytes, "I am a test", user);
+
+        // toString() - tests to see that the object as a string looks how the actual object should look
+        assertThat(p1.toString(), containsString("Profile"));
+
+        // hashcode - like a digital footprint
+        assertThat(p1.hashCode(), is(p2.hashCode()));
+
+        // equals - testing whether p1 is the same and matches whatever is in the bracket
+        assertThat(p1.equals(p2), is(true));
+        assertThat(p1.equals(null), is(false));
+        assertThat(p1.equals(new Object()), is(false));
+    }
+
 }
 
 
